3278e734bb4dc1d2af55b4d2d63aa4b7b10cbec6
Random76520 <tun45218@temple.edu>
2023-04-06 12:21:46 -0400

    (new commit)

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,13 +2,19 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.widget.Button
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
-    val viewPager: ViewPager2 by lazy {
+    var numPages = 0
+
+    private val button: Button by lazy {
+        findViewById(R.id.button)
+    }
+
+    private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
     }
 
@@ -16,8 +22,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button.setOnClickListener {
+            numPages++
+            viewPager.adapter?.notifyItemInserted(numPages - 1)
+
+            viewPager.setCurrentItem(numPages - 1, false)
+        }
+
+        //supportFragmentManager.findFragmentByTag("fPosition")
+
         viewPager.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numPages
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
         }
